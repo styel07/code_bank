54f8bde40d040188766ff66c542f6075f0e8f605
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-02-29 02:02:08 -0800

    modify recursive/recursiveString and add prime

--- recursive/recursiveReverseString.js
+++ recursive/recursiveReverseString.js
@@ -6,3 +6,11 @@ function reverseString(input) {
 }
 
 reverseString("hello");
+
+function reverse (str) {
+  if (str === "") {
+    return "";
+  } else {
+      r eturn reverse(str.substr(1)) + str.charAt(0);
+  }
+}
