22fbeddcaaf6ec59dbc121eaa4545be81f000e8c
Eleazar Corpuz <eleazarcorpuz@gmail.com>
2016-06-22 20:16:57 -0700

    add to ex-arrow-funct

--- ex-arrow-func.js
+++ ex-arrow-func.js
@@ -3,3 +3,27 @@ var names = ['eL', 'mike', 'Nolan'];
 names.forEach(function(name) {
   console.log('forEach', name);
 });
+
+names.forEach((name) => {
+  console.log(name);
+});
+
+names.forEach((name) => console.log(name));
+
+var returnMe = (name) => name+'!';
+
+console.log(returnMe('lahsdkjaksdahsd'));;
+
+// functionadd(a,b) {
+//   return a + b;
+// }
+
+var addStatement = (a,b) =>  {
+  return a + b;
+}
+
+console.log(addStatement(4,7));
+
+var addExpression = (a,b) => a+b;
+
+console.log(addExpression(3,-2));
